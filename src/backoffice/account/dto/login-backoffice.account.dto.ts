a5e4306fd91b024d61761d00a91e243d941aa4fa
oussama seffai <ousf.dev@gmail.com>
2023-11-21 13:23:04 +0100

    refactor(backoffice/account): doc and refactor

--- src/backoffice/account/dto/login-backoffice.account.dto.ts
+++ src/backoffice/account/dto/login-backoffice.account.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength, IsEmail } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(6)
   password: string;
--- src/backoffice/account/repositories/backoffice.account.repository.ts
+++ src/backoffice/account/repositories/backoffice.account.repository.ts
@@ -10,19 +10,40 @@ export class AdminsRepository {
     @InjectModel(Admin.name) private adminModel: Model<AdminDocument>,
   ) {}
 
-  async findOne(userFilterQuery: FilterQuery<Admin>): Promise<Admin> {
-    return this.adminModel.findOne(userFilterQuery);
-  }
-
-  async find(usersFilterQuery: FilterQuery<Admin>): Promise<Admin[]> {
-    return this.adminModel.find(usersFilterQuery);
-  }
-
+  /**
+   * Creates a new admin.
+   * @param user The admin object to be created.
+   * @returns A Promise resolving to the created admin.
+   */
   async create(user: Admin): Promise<Admin> {
     const newUser = new this.adminModel(user);
     return newUser.save();
   }
 
+  /**
+   * Finds a single admin based on the provided filter query.
+   * @param userFilterQuery The filter query to search for an admin.
+   * @returns A Promise resolving to the found admin.
+   */
+  async findOne(userFilterQuery: FilterQuery<Admin>): Promise<Admin> {
+    return this.adminModel.findOne(userFilterQuery);
+  }
+
+  /**
+   * Finds admins based on the provided filter query.
+   * @param usersFilterQuery The filter query to search for admins.
+   * @returns A Promise resolving to an array of found admins.
+   */
+  async find(usersFilterQuery: FilterQuery<Admin>): Promise<Admin[]> {
+    return this.adminModel.find(usersFilterQuery);
+  }
+
+  /**
+   * Finds a single admin and updates it based on the provided filter query and update query.
+   * @param userFilterQuery The filter query to find the admin to update.
+   * @param updateQuery The update query to be applied.
+   * @returns A Promise resolving to the updated admin.
+   */
   async findOneAndUpdate(
     userFilterQuery: FilterQuery<Admin>,
     updateQuery: UpdateQuery<Admin>,
